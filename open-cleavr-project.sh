9ac032ec0244b36cdd07f9d29e79d4387d9e5fd3
Ashok Gelal <ashokgelal@gmail.com>
2022-01-24 15:22:45 -0500

    add open Cleavr Project script

---
+++ open-cleavr-project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Cleavr Project
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🔖
+# @raycast.packageName Dev Utils
+
+# Documentation:
+# @raycast.description Open Cleavr Project on GitHub
+# @raycast.author Ashok Gelal
+
+open 'https://github.com/orgs/LittleBets/projects/1/views/1'
+
