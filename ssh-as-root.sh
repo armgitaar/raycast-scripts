b83a190f4fbcbd2175a6311b75aeb61d69b5bda8
Ashok Gelal <ashokgelal@gmail.com>
2022-01-24 16:03:56 -0500

    tweak icon

--- ssh-as-root.sh
+++ ssh-as-root.sh
@@ -6,7 +6,7 @@
 # @raycast.mode silent
 
 # Optional parameters:
-# @raycast.icon 🤖
+# @raycast.icon 🔐
 # @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
 # @raycast.packageName Dev Utils
 
