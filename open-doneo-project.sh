fcb4f74576f43d5321da029c4327bc9d25d1487a
Ashok Gelal <ashokgelal@gmail.com>
2022-01-27 11:30:25 -0500

    add doneo open project script

---
+++ open-doneo-project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Doneo Project
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🔖
+# @raycast.packageName Dev Utils
+
+# Documentation:
+# @raycast.description Open Doneo Project on GitHub
+# @raycast.author Ashok Gelal
+
+open 'https://github.com/orgs/LittleBets/projects/3/views/1'
+
